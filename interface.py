056ed072923da3b3673ec6ddafec37d43558e4cc
olenapolianska <polyanskaolena2@gmail.com>
2024-02-11 13:15:12 +0200

    homework

--- interface.py
+++ interface.py
@@ -1,12 +1,17 @@
+import requests
+import json
 from PyQt5.QtWidgets import *
+
 
 app = QApplication([])
 
 valcode = QLineEdit()
 date = QLineEdit()
-result = QPushButton("Отримати результат ")
+resultbtn = QPushButton("Отримати результат ")
 valcodelbl = QLabel("Код валюти")
 datelbl = QLabel("Дата")
+resultlbl = QLabel("")
+
 window = QWidget()
 mainline = QVBoxLayout()
 line = QHBoxLayout()
@@ -15,15 +20,22 @@ mainline.addWidget(valcodelbl)
 mainline.addWidget(valcode)
 mainline.addWidget(datelbl)
 mainline.addWidget(date)
-mainline.addWidget(result)
+mainline.addWidget(resultbtn)
+mainline.addWidget(resultlbl)
+
 
 def result():
-    
+    valcode_text = valcode.text()
+    date_text = date.text()
+    a = requests.get(f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={valcode_text}&date={date_text}&json")
+    if a.status_code == 200:
+        data = json.loads(a.text)
+        resultlbl.setText(f" {data[0]['txt']}: {data[0]['rate']}")
 
 
 
 
-
+resultbtn.clicked.connect(result)
 window.setLayout(mainline)
 window.show()
 app.exec()
